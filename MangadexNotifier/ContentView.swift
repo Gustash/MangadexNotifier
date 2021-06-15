492115d92c134d405ac47461b82ce269ded03fc7
Gustavo Parreira <gustavotcparreira@gmail.com>
2021-06-15 11:23:50 +0100

    Initial Commit

---
+++ MangadexNotifier/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MangadexNotifier
+//
+//  Created by Gustavo Parreira on 15/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MangadexNotifier/MangadexNotifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangadexNotifierApp.swift
+//  MangadexNotifier
+//
+//  Created by Gustavo Parreira on 15/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct MangadexNotifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
